43b9a7e049651482633eb9827e4c9c88ae9ae335
stockifab <stockifab@pm.me>
2026-03-22 16:50:03 +0100

    modify installation script to auto-update existing installs

--- install/install-linux.sh
+++ install/install-linux.sh
@@ -13,10 +13,25 @@ TARGET="${DESTINATION}${VERSION}"
 
 ########## SCRIPT ##########
 mkdir -p "/home/$USER/.local/share/typst/packages/local/"
-git clone $LINK $TARGET
-cd $TARGET
-git submodule init
-git submodule update
+
+if [ -d "$TARGET/.git" ]; then
+	echo "Existing installation found at $TARGET"
+	echo "Updating template..."
+	cd "$TARGET"
+	git pull --ff-only
+	git submodule update --init --recursive
+else
+	if [ -d "$TARGET" ]; then
+		echo "Target path exists but is not a git repository: $TARGET"
+		echo "Please remove it manually and run this script again."
+		exit 1
+	fi
+
+	echo "Installing template to $TARGET"
+	git clone "$LINK" "$TARGET"
+	cd "$TARGET"
+	git submodule update --init --recursive
+fi
 
 ########## FINISH ##########
 echo "#################### All done :) ####################"
